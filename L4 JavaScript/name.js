91dd86530bbdb9026580b2ce95455f04aefcb0f0
Mir Rahed Uddin <rahedmir@gmail.com>
2022-04-02 22:10:15 +0530

    Delete L4 JavaScript directory

--- L4 JavaScript/name.js
+++
@@ -1,8 +0,0 @@
-var yourName = prompt("Enter Your Name");
-
-var firstName = yourName.slice(0,1);
-var lastName = yourName.slice(1,yourName.length);
-
-var newName = firstName.toUpperCase() + lastName.toLowerCase();
-
-window.alert("Hello, " + newName);
